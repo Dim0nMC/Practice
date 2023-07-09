51ade33620af90f01c9e992feb2076af3fc51d14
Dim0nMC <108579052+Dim0nMC@users.noreply.github.com>
2023-07-10 00:26:45 +0300

    Add files via upload

---
+++ Task2/EnumerableExtensions.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public static class EnumerableExtensions
+    {
+        public static IEnumerable<int> _GetCombines(
+        this IEnumerable<int> collection, int k)
+        {
+            
+            k = 2;
+            foreach (var item1 in collection)
+            {
+                
+                foreach (var item2 in collection)
+                {
+                    
+                    if(item2 >= item1)
+                    {
+                        yield return item1;
+                        yield return item2;
+                    }
+                        
+                }
+            }
+        }
+
+        public static void GetAllSubstes(
+        this IEnumerable<int> collection)
+        {
+            
+            List<int> a = new List<int>();
+            
+            foreach (var item1 in collection)
+            {
+                a.Add(item1);
+            }
+            for(int i=1; i<=a.Count; i++)
+            {
+                List<int> b = new List<int>();
+                
+                for (int j = 0; j +i <= a.Count; j++)
+                {
+                    Console.Write("[");
+                    for(int l = j; l < j + i; l++)
+                    {
+                        Console.Write(a[l]+",");
+                    }
+                    Console.WriteLine("]");
+                }
+            }
+            
+        }
+
+        public static IEnumerable<int> _GetPermutations(this IEnumerable<int> collection, IList<int> arr,string current="")
+        {
+            
+                if (arr.Count == 0) //если все элементы использованы, выводим на консоль получившуюся строку и возвращаемся
+                {
+                    Console.WriteLine(current);
+                    return collection;
+                }
+                for (int i = 0; i < arr.Count; i++) //в цикле для каждого элемента прибавляем его к итоговой строке, создаем новый список из оставшихся элементов, и вызываем эту же функцию рекурсивно с новыми параметрами.
+                {
+                    List<int> lst = new List<int>(arr);
+                    lst.RemoveAt(i);
+                    lst._GetPermutations(lst, current + arr[i].ToString());
+                }
+            
+            return collection;
+            
+        }
+    }
+}
---
+++ Task2/OwnArray.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class OwnArray:
+        IEnumerable<int>
+    {
+        private List<int> _array;
+
+        public OwnArray()
+        {
+            _array = new List<int>();
+        }
+
+        private OwnArray(
+            List<int> array)
+        {
+            _array = new List<int>(array);
+        }
+
+
+
+       public void Insert(int value, int index)
+        {
+            _array.Insert(index, value);
+            
+
+        }
+
+        public override string ToString()
+        {
+            string s ="";
+            foreach(var item in _array) 
+            {
+                s = s + Convert.ToString(item)+" ";
+            }
+            return s;
+        }
+
+        public void GetCombines()
+        {
+            int i = 0;
+            foreach (var item in _array._GetCombines(2))
+            {
+                if (i % 2 == 0)
+                {
+                    Console.Write("["+ item +", ");
+                }
+                if (i % 2 == 1)
+                {
+                    Console.WriteLine(item + "]");
+                }
+                i++;
+            }
+        }
+        
+        public void GetPermutations(OwnArray myarray)
+        {
+            List<int> a = new List<int>();
+            foreach(var item in myarray) 
+            {
+                a.Add(item);
+            }
+            _array._GetPermutations(a);
+        }
+
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (var arrayItem in _array)
+            {
+                yield return arrayItem;
+            }
+
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        
+    }
+}
