43f565d138ca8dba85b2475e1df56480892579c1
Dim0nMC <108579052+Dim0nMC@users.noreply.github.com>
2023-07-10 18:39:03 +0300

    Add files via upload

---
+++ Task2/MyComparer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    public class MyComparer<T> :
+        IEqualityComparer<T>
+    {
+        private static MyComparer<T>? _instance;
+
+        private MyComparer()
+        {
+
+        }
+
+        public static MyComparer<T> Instance =>
+            _instance ??= new MyComparer<T>();
+        public bool Equals(T? x, T? y)
+        {
+            if (ReferenceEquals(x, y)) return true;
+            if (ReferenceEquals(x, null)) return false;
+            if (ReferenceEquals(y, null)) return false;
+            return x.GetType() == y.GetType() && x.Equals(y);
+        }
+        public int GetHashCode(T? obj)
+        {
+            return obj?.GetHashCode() ?? 0;
+        }
+    }
+}
