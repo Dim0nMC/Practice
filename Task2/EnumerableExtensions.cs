d2849dcf70da4da7e89f8dd223a660ddaf728b6c
Dim0nMC <108579052+Dim0nMC@users.noreply.github.com>
2023-07-10 11:34:08 +0300

    Update EnumerableExtensions.cs

--- Task2/EnumerableExtensions.cs
+++ Task2/EnumerableExtensions.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,8 +8,8 @@ namespace Task2
 {
     public static class EnumerableExtensions
     {
-        public static IEnumerable<int> _GetCombines(
-        this IEnumerable<int> collection, int k)
+        public static IEnumerable<T> _GetCombines<T>(
+        this IEnumerable<T> collection, int k)
         {
             
             k = 2;
@@ -29,11 +29,11 @@ namespace Task2
             }
         }
 
-        public static void GetAllSubstes(
-        this IEnumerable<int> collection)
+        public static void GetAllSubstes<T>(
+        this IEnumerable<T> collection)
         {
             
-            List<int> a = new List<int>();
+            List<T> a = new List<T>();
             
             foreach (var item1 in collection)
             {
@@ -56,22 +56,26 @@ namespace Task2
             
         }
 
-        public static IEnumerable<int> _GetPermutations(this IEnumerable<int> collection, IList<int> arr,string current="")
+        public static void _GetPermutations<T>(this IEnumerable<T> collection, IList<T> arr,string current="")
         {
             
                 if (arr.Count == 0) //если все элементы использованы, выводим на консоль получившуюся строку и возвращаемся
                 {
                     Console.WriteLine(current);
-                    return collection;
+                return;
                 }
                 for (int i = 0; i < arr.Count; i++) //в цикле для каждого элемента прибавляем его к итоговой строке, создаем новый список из оставшихся элементов, и вызываем эту же функцию рекурсивно с новыми параметрами.
-                    List<int> lst = new List<int>(arr);
+                {
+                    List<T> lst = new List<T>(arr);
                     lst.RemoveAt(i);
                     lst._GetPermutations(lst, current + arr[i].ToString());
                 }
-            
-            return collection;
+
+            return;
             
         }
+
+     
+
     }
 }
